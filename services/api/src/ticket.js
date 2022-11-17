fa598c514d1a276a48c7255baa869cba478b824f
Vincenzo Marcella <marcellavincenzo@protonmail.com>
2022-11-17 16:42:04 +0100

    Preparing api resolvers and minor renaming

---
+++ services/api/src/ticket.js
@@ -0,0 +1,29 @@
+"use strict";
+/*const {
+  DynamoDBClient,
+} = require("@aws-sdk/client-dynamodb");
+const { DynamoDBDocumentClient, QueryCommand, PutCommand } = require("@aws-sdk/lib-dynamodb"); 
+*/
+
+const fetch = async (event) => {
+    console.debug("EVENT: ", JSON.stringify(event))
+}
+
+const update = async (event) => {
+    console.debug("UPDATE EVENT: ", JSON.stringify(event))
+}
+
+const open = async (event) => {
+    console.debug("OPEN EVENT: ", JSON.stringify(event))
+}
+
+const close = async (event) => {
+    console.debug("CLOSE EVENT: ", JSON.stringify(event))
+}
+
+module.exports = {
+    fetch,
+    update,
+    open,
+    close
+}
